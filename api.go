479a2bb87f7f8d60c55a6bdb121a1290e554d266
motemen <motemen@gmail.com>
2015-12-16 20:24:59 +0900

    wip

---
+++ api.go
@@ -0,0 +1,223 @@
+package iferr
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"go/ast"
+	"go/parser"
+	"go/token"
+
+	"golang.org/x/tools/go/ast/astutil"
+	"golang.org/x/tools/go/loader"
+	"golang.org/x/tools/go/types"
+
+	"github.com/motemen/go-astmanip"
+)
+
+var (
+	panicCode    = "panic(err.Error())"
+	logFatalCode = "log.Fatal(err)"
+)
+
+func doit() error {
+	flag.Parse()
+
+	conf := loader.Config{
+		Fset:       token.NewFileSet(),
+		ParserMode: parser.ParseComments,
+	}
+	if _, err := conf.FromArgs(flag.Args(), true); err != nil {
+		return err
+	}
+
+	prog, err := conf.Load()
+
+	if err != nil {
+		return err
+	}
+
+	pkgs := prog.InitialPackages()
+	for _, pkg := range pkgs {
+		for _, file := range pkg.Files {
+			rewriteFile(prog.Fset, file, pkg.Info)
+		}
+	}
+
+	return nil
+}
+
+var errorType types.Type
+
+func init() {
+	errorType = types.Universe.Lookup("error").Type()
+	log.SetFlags(log.Lshortfile)
+}
+
+// errorAssign is an assign statement which involves an error-typed variable.
+type errorAssign struct {
+	outerFunc *ast.FuncDecl
+	stmt      *ast.AssignStmt
+	ident     *ast.Ident
+}
+
+func rewriteFile(fset *token.FileSet, f *ast.File, info types.Info) {
+	errAssigns := []errorAssign{}
+
+	ast.Inspect(f, func(node ast.Node) bool {
+		assign, ok := node.(*ast.AssignStmt)
+		if !ok {
+			return true
+		}
+
+		for _, lhs := range assign.Lhs {
+			if ident, ok := lhs.(*ast.Ident); ok && ident.Name != "_" {
+				t := info.TypeOf(ident)
+				if types.Identical(t, errorType) {
+					var funcDecl *ast.FuncDecl
+					path, _ := astutil.PathEnclosingInterval(f, assign.Pos(), assign.End())
+					for _, p := range path {
+						var ok bool
+						funcDecl, ok = p.(*ast.FuncDecl)
+						if ok {
+							break
+						}
+					}
+					if funcDecl != nil {
+						log.Println(fset.Position(assign.Pos()))
+						errAssigns = append(errAssigns, errorAssign{
+							outerFunc: funcDecl,
+							stmt:      assign,
+							ident:     ident,
+						})
+					}
+					break
+				}
+			}
+		}
+
+		return false
+	})
+
+	for _, assign := range errAssigns {
+		assignLine := fset.Position(assign.stmt.Pos()).Line
+		next := astmanip.NextSibling(f, assign.stmt)
+		if next == nil || fset.Position(next.Pos()).Line-assignLine > 1 {
+			log.Println(assign.outerFunc.Name, info.Scopes[assign.outerFunc.Type])
+			log.Println(info.ObjectOf(assign.outerFunc.Name))
+
+			handle := makeErrorHandleStatement(assign, info)
+			ifStmt := makeErrorCatchStatement(assign.ident, handle)
+
+			log.Printf("%#v", ifStmt)
+			assign.outerFunc.Body.List = astmanip.InsertStmtAfter(assign.outerFunc.Body.List, ifStmt, assign.stmt)
+			// assign.outerFunc.Type.Results.List[0].Type
+			// if func has error in result type, return
+			// else if log imported, log.Fatal
+			// else panic
+		}
+	}
+}
+
+func makeErrorHandleStatement(assign errorAssign, info types.Info) ast.Stmt {
+	if funcResults := assign.outerFunc.Type.Results; funcResults != nil {
+		errorPosInReturnTypes := -1
+		for i, rt := range funcResults.List {
+			if types.Identical(info.TypeOf(rt.Type), errorType) {
+				errorPosInReturnTypes = i
+				break
+			}
+		}
+		if errorPosInReturnTypes != -1 {
+			returnValues := make([]ast.Expr, len(funcResults.List))
+			for i, rt := range funcResults.List {
+				if i == errorPosInReturnTypes {
+					// return ..., err, ...
+					returnValues[i] = ast.NewIdent(assign.ident.Name)
+				} else {
+					// return ..., zv, ...
+					zv := makeZeroValue(rt.Type, info.TypeOf(rt.Type))
+					returnValues[i] = zv
+				}
+			}
+			log.Printf("%#v", returnValues)
+			return &ast.ReturnStmt{Results: returnValues}
+		}
+	}
+
+	expr, err := parser.ParseExpr(panicCode)
+	if err != nil {
+		panic(fmt.Sprintf("must not fail: %s while parsing %q", err, panicCode))
+	}
+
+	return &ast.ExprStmt{X: expr}
+}
+
+var ifTemplate = `package P; func _() { if err != nil {} }`
+
+func makeErrorCatchStatement(errName *ast.Ident, stmt ast.Stmt) *ast.IfStmt {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "var ifTemplate", ifTemplate, 0)
+	if err != nil {
+		panic(fmt.Sprintf("must not fail: %s", err))
+	}
+
+	// must not fail
+	ifStmt := f.Decls[0].(*ast.FuncDecl).Body.List[0].(*ast.IfStmt)
+	ifStmt.Body.List = []ast.Stmt{stmt}
+
+	astmanip.NormalizePos(ifStmt)
+
+	return ifStmt
+}
+
+func makeZeroValue(e ast.Expr, t types.Type) ast.Expr {
+	switch t := t.(type) {
+	case *types.Basic:
+		switch {
+		case t.Info()&types.IsNumeric != 0:
+			return &ast.BasicLit{
+				Kind:  token.INT,
+				Value: "0",
+			}
+
+		case t.Info()&types.IsString != 0:
+			return &ast.BasicLit{
+				Kind:  token.STRING,
+				Value: `""`,
+			}
+
+		case t.Info()&types.IsBoolean != 0:
+			return ast.NewIdent("false")
+		}
+
+		panic(fmt.Sprintf("makeZeroValue: unexpected basic type: %v", t))
+
+	case *types.Tuple:
+		panic("makeZeroValue: unexpected *types.Tuple")
+
+	case *types.Named:
+		return makeZeroValue(e, t.Underlying())
+
+	case *types.Array:
+		return &ast.CompositeLit{Type: e}
+	case *types.Struct:
+		return &ast.CompositeLit{Type: e}
+
+	case *types.Map:
+		return ast.NewIdent("nil")
+	case *types.Signature:
+		return ast.NewIdent("nil")
+	case *types.Interface:
+		return ast.NewIdent("nil")
+	case *types.Pointer:
+		return ast.NewIdent("nil")
+	case *types.Slice:
+		return ast.NewIdent("nil")
+	case *types.Chan:
+		return ast.NewIdent("nil")
+	}
+
+	panic(fmt.Sprintf("makeZeroValue: unexpected type: %v", t))
+}
